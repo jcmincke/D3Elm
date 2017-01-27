e917fca47f4ff9027486f0348fbfdc00fd918578
Jean-Christophe Mincke <jeanchristophe.mincke@gmail.com>
2017-01-27 22:44:24 +0100

    build tree

--- src/D3Elm/Hierarchy/Tree/Tree.elm
+++ src/D3Elm/Hierarchy/Tree/Tree.elm
@@ -1,7 +1,9 @@
 module D3Elm.Hierarchy.Tree.Tree exposing (..)
 
 import Lazy as L
-import List exposing (append, foldl)
+import List exposing (append, foldl, reverse)
+import Dict exposing (insert, empty, Dict)
+
 
 type alias NodeInfo data = {
   nodeData : data
@@ -13,36 +15,83 @@ type Node d =
   Node Int (L.Lazy (Maybe Node)) (NodeInfo d) (List (Node d))
   |Leaf Int (L.Lazy (Maybe Node)) (NodeInfo d)
 
+getInfo : Node d -> NodeInfo d
+getInfo n =
+  case n of
+    (Leaf _ _ info) -> info
+    (Node _ _ info _) -> info
+
+getIndex : Node d -> Int
+getIndex n =
+  case n of
+    (Leaf i _ _) -> i
+    (Node i _ _ _) -> i
+
+getData : Node d -> d
+getData n = (getInfo n).nodeData
+
+-- construct a Node
+
+buildTree : a -> (a -> d) -> (a -> List a) -> Node d
+buildTree a fvalue fchildren =
+  let noParent = (L.lazy (\() -> Nothing))
+      proc index depth a =
+        let n =
+          case fchildren a of
+            [] -> Leaf index noParent (NodeInfo (fvalue a) depth 0)
+            children ->
+              let (ns, index1) = foldl (\a (acc, i) -> (proc i (depth+1) a::acc, i+1)) ([], index+1) children
+              in Node index1 noParent (NodeInfo (fvalue a) depth 0) (reverse ns)
+        in n
+  in proc 0 0 a
+
+parents : Node d -> Dict Int (Node d)
+parents n =
+  let proc acc n =
+    case n of
+      (Leaf _ _ _) -> acc
+      (Node i _ _ cs) -> foldl (\c acc -> insert (getIndex c) n acc) acc cs
+  in foldBreadthFirst proc empty n
 
 
-foldBreadthFirst : (acc -> Int -> d -> acc) -> acc -> Node d -> acc
+
+--
+--setParents : Node d -> Node d
+--setParents n =
+--  let proc p n =
+--    case n of
+--      (Leaf i _ info) -> Leaf i p info
+--      (Node i _ info cs) -> let n1 = Node i p info
+
+
+foldBreadthFirst : (acc -> Node d -> acc) -> acc -> Node d -> acc
 foldBreadthFirst f acc node =
   let proc acc nodes =
     case nodes of
       [] -> acc
-      (Leaf i _ info :: rest) -> proc (f acc i info.nodeData) rest
-      (Node i _ info children :: rest) -> proc (f acc i info.nodeData) (append rest children)
+      (Leaf _ _ _ as n) :: rest -> proc (f acc n) rest
+      (Node _ _ _ children as n) :: rest -> proc (f acc n) (append rest children)
   in proc acc [node]
 
 
-foldPostOrder : (acc -> Int -> d -> acc) -> acc -> Node d -> acc
+foldPostOrder : (acc -> Node d -> acc) -> acc -> Node d -> acc
 foldPostOrder f acc node =
   let proc acc node =
     case node of
-      (Leaf i _ info) -> f acc i info.nodeData
-      (Node i _ info children) ->
+      (Leaf _ _ _ as n) -> f acc n
+      (Node _ _ _ children as n) ->
         let acc1 = foldl (\n lacc -> proc lacc n) acc children
-        in f acc1 i info.nodeData
+        in f acc1 n
   in proc acc node
 
 
-foldPreOrder : (acc -> Int -> d -> acc) -> acc -> Node d -> acc
+foldPreOrder : (acc -> Node d -> acc) -> acc -> Node d -> acc
 foldPreOrder f acc node =
   let proc acc node =
     case node of
-      (Leaf i _ info) -> f acc i info.nodeData
-      (Node i _ info children) ->
-        let acc1 = f acc i info.nodeData
+      (Leaf _ _ _ as n) -> f acc n
+      (Node _ _ _ children as n) ->
+        let acc1 = f acc n
         in foldl (\n lacc -> proc lacc n) acc1 children
   in proc acc node
 
--- tests/TestD3Elm/D3Elm/Hierarchy/Tree/Tree.elm
+++ tests/TestD3Elm/D3Elm/Hierarchy/Tree/Tree.elm
@@ -5,7 +5,7 @@ import Expect
 import Fuzz exposing (list, int, tuple, string)
 import String
 import Lazy as L
-import List exposing (reverse)
+import List exposing (reverse, all)
 
 
 import D3Elm.Hierarchy.Tree.Tree exposing (..)
@@ -47,24 +47,68 @@ all =
         [ test "foldBreadthFirst" <| testFoldBreadthFirst
           , test "foldPostOrder" <| testFoldPostOrder
           , test "foldPreOrder" <| testFoldPreOrder
+          , test "buildTree" <| testBuildTree
         ]
 
 
 testFoldBreadthFirst () = let
-    r = reverse <| foldBreadthFirst (\acc _ d -> d::acc) [] node
+    r = reverse <| foldBreadthFirst (\acc n -> getData n::acc) [] node
   in Expect.equal r ["F", "B", "G", "A", "D", "I", "C", "E", "H"]
 
 
 testFoldPostOrder () = let
-    r = reverse <| foldPostOrder (\acc _ d -> d::acc) [] node
+    r = reverse <| foldPostOrder (\acc n -> getData n::acc) [] node
   in Expect.equal r ["A", "C", "E", "D", "B", "H", "I", "G", "F"]
 
 testFoldPreOrder () = let
-    r = reverse <| foldPreOrder (\acc _ d -> d::acc) [] node
+    r = reverse <| foldPreOrder (\acc n -> getData n::acc) [] node
   in Expect.equal r ["F", "B", "A", "D", "C", "E", "G", "I", "H"]
 
 
+testBuildTree () =
+  let fchildren n =
+        case n of
+        (Node _ _ _ cs) -> cs
+        _ -> []
+      fvalue n =
+        case n of
+        (Node _ _ info _) -> info.nodeData
+        (Leaf _ _ info) -> info.nodeData
+      t1 = buildTree node fvalue fchildren
+      t2 = buildTree t1 fvalue fchildren
+      t3 = buildTree t2 fvalue fchildren
+  in Expect.true "" <| nodeStructEq t2 t3
+
+
+nodeStructEq : Node d -> Node d -> Bool
+nodeStructEq a b =
+  let eqList la lb =
+        case (la, lb) of
+          ([], []) -> True
+          (a::ra, b::rb) -> nodeStructEq a b && eqList ra rb
+          _ -> False
+  in case (a,b) of
+      (Leaf i1 _ info1, Leaf i2 _ info2) -> i1 == i2 && info1 == info2
+      (Node i1 _ info1 cs1, Node i2 _ info2 cs2) ->
+        let b1 = i1 == i2 && info1 == info2
+        in  if b1
+            then eqList cs1 cs2
+            else False
+      _ -> False
+
+
 {-
+
+
+type Node d =
+  Node Int (L.Lazy (Maybe Node)) (NodeInfo d) (List (Node d))
+  |Leaf Int (L.Lazy (Maybe Node)) (NodeInfo d)
+
+
+
+buildTree : a -> (a -> d) -> (a -> List a) -> Node d
+
+
         , describe "Fuzz test examples, using randomly generated input"
             [ fuzz (list int) "Lists always have positive length" <|
                 \aList ->
