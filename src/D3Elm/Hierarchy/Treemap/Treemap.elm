a33f7388de728a645fadd0bd51891a6e50c58cbd
Jean-Christophe Mincke <jeanchristophe.mincke@gmail.com>
2017-01-31 18:44:27 +0100

    treemap without ratio

--- src/D3Elm/Hierarchy/Treemap/Treemap.elm
+++ src/D3Elm/Hierarchy/Treemap/Treemap.elm
@@ -28,46 +28,28 @@ sliceAndDice : Float -> (Node d -> Float) -> Node d -> Dict Int ((Float, Float),
 sliceAndDice ratio cost node =
   let defBox = ((0,0), (1,1))
       valueDic = sum cost (+) 0 node
-      procV ((x1_, y1_), (x2_, y2_)) boxDic n =
-        let xc = (x1_+x2_)/2
-            dx = (x2_-x1_)/2 * ratio
-            yc = (y1_+y2_)/2
-            dy = (y2_-y1_)/2 * ratio
-
-            x1 = xc - dx * ratio
-            x2 = xc + dx * ratio
-            y1 = yc - dy * ratio
-            y2 = yc + dy * ratio
-        in  case n of
-            Leaf i _ -> D.insert i ((x1, y1), (x2, y2)) boxDic
-            Node i _ cs ->
-              let iweights = L.map (\n -> (getIndex n, getVal 0 valueDic n)) cs
-                  ixs = splitBox x1 x2 iweights
-                  boxDic1 = L.foldl (\(i, xa, xb) dic -> D.insert i ((xa, y1), (xb, y2)) dic) boxDic ixs
-                  foldProc n boxDic =
-                    let box = getVal defBox boxDic n
-                    in procH box boxDic n
-              in L.foldl foldProc boxDic1 cs
-      procH ((x1_, y1_), (x2_, y2_)) boxDic n =
-        let xc = (x1_+x2_)/2
-            dx = (x2_-x1_)/2 * ratio
-            yc = (y1_+y2_)/2
-            dy = (y2_-y1_)/2 * ratio
-
-            x1 = xc - dx * ratio
-            x2 = xc + dx * ratio
-            y1 = yc - dy * ratio
-            y2 = yc + dy * ratio
-        in  case n of
-            Leaf i _ -> D.insert i ((x1, y1), (x2, y2)) boxDic
-            Node i _ cs ->
-              let iweights = L.map (\n -> (getIndex n, getVal 0 valueDic n)) cs
-                  ixs = splitBox y1 y2 iweights
-                  boxDic1 = L.foldl (\(i, ya, yb) dic -> D.insert i ((x1, ya), (x2, yb)) dic) boxDic ixs
-                  foldProc n boxDic =
-                    let box = getVal defBox boxDic n
-                    in procV box boxDic n
-              in L.foldl foldProc boxDic1 cs
+      procV ((x1, y1), (x2, y2)) boxDic n =
+        case n of
+          Leaf i _ -> D.insert i ((x1, y1), (x2, y2)) boxDic
+          Node i _ cs ->
+            let iweights = L.map (\n -> (getIndex n, getVal 0 valueDic n)) cs
+                ixs = splitBox x1 x2 iweights
+                boxDic1 = L.foldl (\(i, xa, xb) dic -> D.insert i ((xa, y1), (xb, y2)) dic) boxDic ixs
+                foldProc n boxDic =
+                  let box = getVal defBox boxDic n
+                  in procH box boxDic n
+            in L.foldl foldProc boxDic1 cs
+      procH ((x1, y1), (x2, y2)) boxDic n =
+        case n of
+        Leaf i _ -> D.insert i ((x1, y1), (x2, y2)) boxDic
+        Node i _ cs ->
+          let iweights = L.map (\n -> (getIndex n, getVal 0 valueDic n)) cs
+              ixs = splitBox y1 y2 iweights
+              boxDic1 = L.foldl (\(i, ya, yb) dic -> D.insert i ((x1, ya), (x2, yb)) dic) boxDic ixs
+              foldProc n boxDic =
+                let box = getVal defBox boxDic n
+                in procV box boxDic n
+          in L.foldl foldProc boxDic1 cs
   in procV defBox empty node
 
 
