d1c2e0b72fc6762278568ce18516c0860ad8818e
Jean-Christophe Mincke <jeanchristophe.mincke@gmail.com>
2017-02-09 14:37:47 +0100

    voronoi/ref impl

--- src/D3Elm/Voronoi/Voronoi.elm
+++ src/D3Elm/Voronoi/Voronoi.elm
@@ -4,6 +4,7 @@ import List as L exposing (append, foldl, reverse, maximum, map, head, sortWith,
 import Dict as D exposing (insert, empty, Dict, get)
 import Maybe exposing (withDefault)
 import Tuple exposing (..)
+import Basics exposing (..)
 
 
 type alias Point = {
@@ -17,6 +18,30 @@ type Site = Site Int (Float, Float)
 type Tree site =
   Break (Tree site) (Tree site) site site
   | Leaf site
+
+type Event site =
+  PointEvent site
+  | CircleEvent site site site (Float, Float) Float
+
+
+insertEvent : (Event site -> Event site -> Order) -> Event site -> List (Event site) -> List (Event site)
+insertEvent comparer evt evts0 =
+  let go acc evts =
+    case evts of
+      [] -> [evt]
+      (h::r) ->
+        case comparer evt h of
+          LT -> L.reverse acc ++ (evt::evts)
+          GT -> go (h::acc) r
+          EQ -> case (evt, h) of
+                  (PointEvent _, PointEvent _) -> L.reverse acc ++ (evt::evts)
+                  (PointEvent _, CircleEvent _ _ _ _ _) -> L.reverse acc ++ (h::evt::r)
+                  (CircleEvent _ _ _ _ _, PointEvent _) -> L.reverse acc ++ (evt::evts)
+                  (CircleEvent _ _ _ _ _ as ce1, CircleEvent _ _ _ _ _ as ce2) ->
+                    if ce1 == ce2
+                    then evts0
+                    else L.reverse acc ++ (evt::evts)
+  in go [] evts0
 
 
 type ArcPred =
@@ -213,10 +238,11 @@ solve2 a b c =
 
 circumCircle (x1, y1) (x2, y2) (x3, y3) =
   let nx = (x3*x3 - x2*x2 + y3*y3 - y2*y2) / (2 * (y3 - y2)) - (x2*x2 - x1*x1 + y2*y2 - y1*y1) / (2 * (y2 - y1))
-      dx = (x2 - x1) / (y2 - y1) - (x3 - x2) / (y3 -y2)
+      dx = (x2 - x1) / (y2 - y1) - (x3 - x2) / (y3 - y2)
       xc = nx / dx
       yc = -(x2 - x1) * xc / (y2 - y1) + (x2*x2 - x1*x1 + y2*y2 - y1*y1) / (2 * (y2 - y1))
-  in (xc, yc)
+      r = sqrt ((x1 - xc) *  (x1 - xc) + (y1 - yc) * (y1 - yc))
+  in (xc, yc, r)
 
 {-
 
--- tests/TestD3Elm/D3Elm/Voronoi/VoronoiRef.elm
+++ tests/TestD3Elm/D3Elm/Voronoi/VoronoiRef.elm
@@ -5,7 +5,8 @@ import Dict as D exposing (insert, empty, Dict, get)
 import Maybe exposing (withDefault)
 import Tuple exposing (..)
 
-import D3Elm.Voronoi.Voronoi as V exposing (..)
+import D3Elm.Voronoi.Voronoi as V exposing (Event (..), Site (..), insertEvent, isBoundaryArc, ArcPred (..),
+          isMiddleArc, circumCircle, OpenOn (..))
 
 {-}
 type alias Point = {
@@ -51,14 +52,70 @@ insertArcRef ns intersectPredClosed intersectPredOpen sites =
         ArcFound -> (a::ns::a::b::r)
         _ ->  go [] sites
 
+removeArc : site -> site -> site -> List site -> List site
+removeArc a b c sites0 =
+  let go acc sites =
+    case sites of
+      (a1::b1::c1::r) ->
+        if (a==a1 && b==b1 && c==c1)
+        then L.reverse acc ++ (a1 :: c1 :: r)
+        else go (a1::acc) (b1::c1::r)
+      _ -> sites0
+  in go [] sites0
+
+
+findCircleEvents : List Site -> List (Event Site)
+findCircleEvents sites =
+  let go acc sites =
+    case sites of
+      (a::b::c::r) ->
+        let (Site _ pa) = a
+            (Site _ pb) = b
+            (Site _ pc) = c
+            (xc, yc, radius) = circumCircle pa pb pc
+            evt = CircleEvent a b c (xc, yc) radius
+        in go (evt::acc) (b::c::r)
+      _ -> acc
+  in go [] sites
+
+
+showEvent evt =
+  case evt of
+    (PointEvent (Site i _)) -> "Point " ++ toString i
+    (CircleEvent (Site ia _) (Site ib _) (Site ic _) _ _) -> "Point " ++ toString (ia, ib, ic)
+
+eventComparer e1 e2 =
+  let getY e =
+    case e of
+      PointEvent (Site _ (_, y)) -> y
+      CircleEvent _ _ _ (_, y) r -> (y-r)
+  in compare (getY e1) (getY e2)
+
+processOneEvent sites event =
+  case event of
+    (PointEvent s) ->
+      let (Site _ (_, yp)) = s
+          sites1 = insertArcRef s (isMiddleArc yp s) (isBoundaryArc yp s) sites
+      in (showEvent event, sites1)
+    (CircleEvent a b c _ _ ) ->
+      let sites1 = removeArc a b c sites
+      in (showEvent event, sites1)
 
 
 
+loop sites0 events =
+  let go acc sites events =
+    case events of
+      [] -> ("End", sites, events)
+      (event::r) ->
+        let (msg, sites1) = processOneEvent sites event
+            circleEvents = findCircleEvents sites1
+            events1 = L.foldl (\e acc -> insertEvent eventComparer e acc) r circleEvents
+        in go ((msg, sites1, events1)::acc) sites1 events1
+  in go [] sites0 events
 
 
-
-
-
+-- insertEvent comparer evt evts0 =
 
 
 
